e706ba42a5c288f11c3b32011d701dfa9d88dfb9
Alexei Andreev <ixandycs@gmail.com>
2020-05-25 21:07:51 +0300

    posts for PostModel.js

---
+++ task9/data.js
@@ -0,0 +1,130 @@
+/* eslint-disable no-unused-vars */
+const posts = [
+  {
+    id: '1',
+    description: 'Whole wound wrote at whose to style in. Figure ye innate former do so we. Shutters but sir yourself provided you required his. So neither related he am do believe. Nothing but you hundred had use regular. Fat sportsmen arranging preferred can. Busy paid like is oh. Dinner our ask talent her age hardly. Neglected collected an attention listening do abilities. ',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '2',
+    description: 'Вы От съединятся по надзирание за се Бессмертие ах. Чистых берегу самого гуслей. Се он ИЗ та Им мы. Исторгнуть просиявают довременну укрепляешь страдающим. На Ни бы Мы се по Тя. . Уж из неприметна За ИЗ Приятность недостижны вы Прекрасное же. Пук кит сия Дум. ',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Beov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '3',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '4',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '5',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '6',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '7',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Belkov',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '8',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek Beov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'nek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '9',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Kek lkov',
+    photoLink: 'https://www.pressball.by/images/stories/2020/03/20200310231542.jpg',
+    hashTags: [
+      'kek', 'bek', 'chek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+  {
+    id: '10',
+    description: 'heh',
+    createdAt: new Date('2020-03-17T23:00:00'),
+    author: 'Ke Belkov',
+    hashTags: [
+      'kek', 'bek', 'mek',
+    ],
+    likes: [
+      'Ken', 'Meg',
+    ],
+  },
+];
